2ed4474c4f2fb54c99760ccebbe49a3b782742c0
Fatima A. Rodriguez <fatimux.r@gmail.com>
2026-02-13 03:20:44 +0000

    added handle form submissio, form values

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+
+//Get all needed DOM elements
 const form = document.getElementById("checkInForm");
 const nameInput = document.getElementById("attendeeName");
 const teamSelect = document.getElementById("teamSelect");
+
+//Handle form submission
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    //get form values
+    const name = nameInput.value;
+    const team = teamSelect.value;
+    console.log(`Name: ${name}, Team: ${team}`);
+});
